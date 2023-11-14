61e418211dbabe0d8cf4b9e9a1a4c0821beddb91
Hocheol <didifia9850@naver.com>
2023-11-14 11:51:15 +0900

    my

---
+++ Vue/08-01-vue-with-drf/vue-project/src/views/ArticleView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Article Page</h1>
+    <ArticleList />
+    <RouterLink :to="{ name:'CreateView'}">
+      [Create]
+    </RouterLink>
+  </div>
+</template>
+
+<script setup>
+// 최상단 훅인 ArticleView.vue에 mounted 시켜야 한다.
+import { onMounted } from 'vue';
+import { useCounterStore } from '@/stores/counter'
+import { RouterLink } from 'vue-router'
+import ArticleList from '@/components/ArticleList.vue'
+
+
+const store = useCounterStore()
+
+onMounted(() => {
+  store.getArticles()
+})
+
+</script>
+
+<style>
+
+</style>
